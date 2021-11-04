d7f7570341c54e636b8b23ae9c8fc73b7428ca51
Margegaj Fatmir <fatmir.margegaj@getabstract.com>
2021-11-04 14:41:06 +0100

    addds

--- src/main/java/com/getabstract/springDemo/MyController.java
+++ src/main/java/com/getabstract/springDemo/MyController.java
@@ -3,13 +3,15 @@ package com.getabstract.springDemo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
 	@GetMapping("/greeting")
-	public String greeting(Model model) {
+	public String greeting(@RequestParam(required = false, defaultValue = "Fatmiryy")String firstName, Model model) {
 		String a = "joooo";
 		model.addAttribute("a", a);
+		model.addAttribute("firstName", firstName);
 		return "greeting";
 		
 	}
