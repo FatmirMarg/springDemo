99f5739761a01cbcfec3dce4f3471a79e3f5a8a5
Margegaj Fatmir <fatmir.margegaj@getabstract.com>
2021-11-18 10:03:22 +0100

    tascenrechner mit operationszeichen und manueller angabe

--- src/main/java/com/getabstract/springDemo/MyController.java
+++ src/main/java/com/getabstract/springDemo/MyController.java
@@ -6,77 +6,90 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class MyController {
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(required = false, defaultValue = "Fatmiryy")String firstName, Model model) {
-		String a = "joooo";
-		model.addAttribute("a", a);
-		model.addAttribute("firstName", firstName);
+public class MyController
+{
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(required = false, defaultValue = "Fatmiryy") String firstName, Model model)
+    {
+        String a = "joooo";
+        model.addAttribute("a", a);
+        model.addAttribute("firstName", firstName);
 
-		return "greeting";
-		
-	}
-	
-	@GetMapping("/pets")
-	public String pets(@RequestParam(required = false, defaultValue = "geben Sie bitte einen Hundenamen ein!!")String hund, Model model) {
-		model.addAttribute("hund", hund);
-		return "pets"; }
-		
-		
-	@GetMapping("/add")
-	public String add(@RequestParam(required = false)int num1, int num2, Model model) {
-		
-		model.addAttribute("num1", num1);
-		model.addAttribute("num2", num2);
-		int result = num1 + num2;
-		model.addAttribute("result", result);
-		return "add";
-		
-	}
-	
-	@GetMapping("/sub")
-	public String sub(@RequestParam(required = false)int num1, int num2, Model model) {
-		
-		model.addAttribute("num1", num1);
-		model.addAttribute("num2", num2);
-		int result = num1 - num2;
-		model.addAttribute("result", result);
-		return "sub";
-	}
-	
-	
-	
-	
-	
-	
-	
-	@GetMapping("/calc")
-	public String AddOrSub(
-			@RequestParam(required = false) int num1,
-			@RequestParam(required = false) int num2, 
-			@RequestParam(required = false) String op,
-			Model model) {
-		
-	
-		
-		int result = 9;
-		String operator = "x";
-		
-		if(op.equals ("add")) {
-			result = num1 + num2;
-			operator = "+";
-		} else if(op.equals  ("sub")) {
-			result = num1 - num2;
-			operator = "-";
-		} else {
-			System.out.println("Geben sie einen richtigen Wert ein");
-		}		
-		model.addAttribute("num1", num1);
-		model.addAttribute("num2", num2);
-		model.addAttribute("op" , op);
-		model.addAttribute("result", result);
-		model.addAttribute("operator", operator);
+        return "greeting";
 
-		return "calc";
-	}
+    }
+
+    @GetMapping("/pets")
+    public String pets(
+            @RequestParam(required = false, defaultValue = "geben Sie bitte einen Hundenamen ein!!") String hund,
+            Model model)
+    {
+        model.addAttribute("hund", hund);
+        return "pets";
+    }
+
+    @GetMapping("/add")
+    public String add(@RequestParam(required = false) int num1, int num2, Model model)
+    {
+
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        int result = num1 + num2;
+        model.addAttribute("result", result);
+        return "add";
+    }
+
+    @GetMapping("/sub")
+    public String sub(@RequestParam(required = false) int num1, int num2, Model model)
+    {
+
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        int result = num1 - num2;
+        model.addAttribute("result", result);
+        return "sub";
+    }
+
+    @GetMapping("/calc")
+    public String AddOrSub(
+            @RequestParam(required = false, defaultValue = "0") int num1,
+            @RequestParam(required = false, defaultValue = "0") int num2,
+            @RequestParam(required = false, defaultValue = "") String add,
+            @RequestParam(required = false, defaultValue = "") String sub,
+            @RequestParam(required = false, defaultValue = "") String multi,
+            @RequestParam(required = false, defaultValue = "") String div,
+
+            Model model)
+    {
+        String operator2 = "";
+        int result = 1;
+
+        if (add.equals("+"))
+        {
+            result = num1 + num2;
+            operator2 = "+";
+        }
+        else if (sub.equals("-"))
+        {
+            result = num1 - num2;
+            operator2 = "-";
+        }
+        else if (multi.equals("*"))
+        {
+            result = num1 * num2;
+            operator2 = "*";
+        }
+        else if (div.equals("/"))
+        {
+            result = num1 / num2;
+            operator2 = "/";
+        }
+
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        model.addAttribute("result", result);
+        model.addAttribute("operator2", operator2);
+
+        return "calc";
+    }
 }
