4a442ea3c78e51180ecbbfb04bfd4adc505c684f
Margegaj Fatmir <fatmir.margegaj@getabstract.com>
2021-11-25 17:19:26 +0100

    did some css changes

--- src/main/resources/static/calculator.js
+++ src/main/resources/static/calculator.js
@@ -5,21 +5,23 @@ function addEventListener(element){
 }
 
 function onClick(event){
-  const userInput = event.target.value;
+	const userInput = event.target.value;
  	const operator = document.querySelector(".operator").value;
 
-  if(isNaN(userInput) && userInput != "="){
-    const operatorInput = document.querySelector(".operator");
-    operatorInput.value = userInput;
-  }
-  else if(operator != "+" && operator != "-" && operator != "*" && operator != "/" )
-  {
-  	const firstNumber = document.querySelector(".firstNumber");
-    firstNumber.value = firstNumber.value + userInput;
-  } else if (userInput != "=" && (operator == "+" || operator == "-" || operator == "*" || operator == "/" )){
-	const secondNumber = document.querySelector(".secondNumber");
-    secondNumber.value = secondNumber.value + userInput;
-}
-
+	if(isNaN(userInput) && userInput != "=")
+	{
+    	const operatorInput = document.querySelector(".operator");
+    	operatorInput.value = userInput;
+ 	}
+	else if(operator != "+" && operator != "-" && operator != "*" && operator != "/" )
+	{
+  		const firstNumber = document.querySelector(".firstNumber");
+    	firstNumber.value = firstNumber.value + userInput;
+	}
+	else if (userInput != "=" && (operator == "+" || operator == "-" || operator == "*" || operator == "/" ))
+	{
+		const secondNumber = document.querySelector(".secondNumber");
+    	secondNumber.value = secondNumber.value + userInput;
+	}
 }
 
